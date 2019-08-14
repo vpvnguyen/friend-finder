90afd3d62360b1d949219abe6fb074128c471882
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-14 10:25:44 -0700

    npm express; setup express, port, middleware, and sample routing

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var PORT = process.env.PORT || 3000;
+
+// express middleware; parse json
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+// routing
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/home.html"));
+});
+
+// sample express post
+app.post('/profile', function (req, res, next) {
+    console.log(req.body)
+    res.json(req.body)
+  })
+
+// listen for route
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
