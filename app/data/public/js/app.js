5479e063832bf2bff875fa17a3cbdbc1bb361672
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-16 00:33:38 -0700

    pulled form input

--- app/data/public/js/app.js
+++ app/data/public/js/app.js
@@ -31,7 +31,19 @@ $("form").submit(function (event) {
     // console.log($('.form-control').val());
     var name = $('#name').val();
     var image = $('#image').val();
-    console.log(name, image)
+    var q1 = $('#q1').val();
+    var q2 = $('#q2').val();
+    var q3 = $('#q3').val();
+    var q4 = $('#q4').val();
+    var q5 = $('#q5').val();
+    var q6 = $('#q6').val();
+    var q7 = $('#q7').val();
+    var q8 = $('#q8').val();
+    var q9 = $('#q9').val();
+    var q10 = $('#q10').val();
+
+    console.log(name, image, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10)
+
 });
 
 // constructor function called "Programmer" which can be used to create objects containing information on various programmers.
