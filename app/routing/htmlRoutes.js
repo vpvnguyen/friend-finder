9c6289a7ba0f1e3e9ec61053f24cde8797afad32
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-18 22:41:49 -0700

    es6 arrow functions

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
     });
 
     // catch all routing
-    app.get('*', function (req, res) {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 };
